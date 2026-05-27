37a94b4ca69146714dd8de99eb9a463dfe412410
Luz Moreno Campos <luzdirection@gmail.com>
2026-05-27 17:11:19 -0500

    created footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Header from "./components/Header";
 import {Routes, Route} from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
   return (
   <>
-
-
   <Header />
+  <Footer />
   </>
   );
 }
