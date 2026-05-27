3306cd52cdd61886b5c11ae7291035576baccdb3
Luz Moreno Campos <luzdirection@gmail.com>
2026-05-27 16:14:26 -0500

    added logo file and created header  component  function  and  imports

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { IoCartOutline } from "react-icons/io5";
+import storeLogo from "../assets/media/store-logo.png"
+
+function Header(props) {
+  return (
+    <header>
+  
+    </header>
+  );
+}
+
+export default Header;
