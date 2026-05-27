7a776e69b01e635a5632f202696357f679deef20
Luz Moreno Campos <luzdirection@gmail.com>
2026-05-27 16:27:45 -0500

    created header jsx

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,8 +5,18 @@ import storeLogo from "../assets/media/store-logo.png"
 
 function Header(props) {
   return (
-    <header>
-  
+    <header className="header">
+      <div className="header-logo">
+        <img src={storeLogo} alt="Store Logo" />
+      </div>
+      <nav className="header-nav">
+        <div className="header-links">
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+        <Link to="/cart" className="header-cart"> <IoCartOutline size={24} /> </Link>
+      </nav>
     </header>
   );
 }
