2bf4c33395b726ea5243377a248915a141c5ef1e
Luz Moreno Campos <luzdirection@gmail.com>
2026-05-28 15:00:17 -0500

    moved fetching logic from home to app.js to be able to pass products to poduct page. added home jsx with props and routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import {Routes, Route} from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import HeroBanner from "./components/HeroBanner";
-import CategorySelector from "./components/CategorySelector";
-import SortingDropdown from "./components/SortingDropdown";
+import {Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+
 
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setLoading(true);
+
+        const menRes = await axios.get("https://fakestoreapi.com/products/category/men's%20clothing");
+        const womenRes = await axios.get("https://fakestoreapi.com/products/category/women's%20clothing");
+
+        const clothing = [...menRes.data, ...womenRes.data];
+        setProducts(clothing);
+
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+  /*
+    if (loading) {
+      return <p>Loading products...</p>;
+    }
+  
+    if (error) {
+      return <p >{error}</p>;
+    }
+  */
 
   return (
-  <>
-  <Header />
-  <HeroBanner />
-  <CategorySelector />
-  <SortingDropdown />
-  <Footer />
-  </>
+    <>
+      {loading && <p>Loading products...</p>}
+      {error && <p>{error}</p>}
+
+      {!loading && !error && (
+        <Routes>
+          <Route path="/" element={<Home products={products} />} />
+          <Route path="/product/:id" element={<Product products={products} />} />
+          { /*
+          I added a temporary wildcard route because I installed gh-pages and the deploy script,
+          which makes React Router expect a sub-path during deployment.
+          This fallback prevents blank screens until the final deploy setup is done.
+          */}
+          <Route path="*" element={<Home products={products} />} />
+        </Routes>
+      )}
+    </>
+
   );
+
 }
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,44 +1,18 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import {useState} from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import HeroBanner from "../components/HeroBanner";
+import CategorySelector from "../components/CategorySelector";
+import SortingDropdown from "../components/SortingDropdown";
+import ProductGallery from "../components/ProductGallery";
+/*import SecondaryBanner from "../components/SecondaryBanner"; ADD IT ONCE IT IS CREATED AND PUT IT IN THE JSX*/ 
 
-function Home() {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
+function Home(props) {
 
   const [sortOption, setSortOption] = useState(null);
   const [category, setCategory] = useState("all");
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        setLoading(true);
-
-        const menRes = await axios.get("https://fakestoreapi.com/products/category/men's%20clothing");
-        const womenRes = await axios.get("https://fakestoreapi.com/products/category/women's%20clothing");
-
-        const clothing = [...menRes.data, ...womenRes.data];
-        setProducts(clothing);
-
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchProducts();
-  }, []);
-
-  if (loading) {
-    return <p>Loading products...</p>;
-  }
-
-  if (error) {
-    return <p >{error}</p>;
-  }
-
-  const filteredProducts = category === "all" ? products : products.filter(prod => prod.category === category);
+  const filteredProducts = category === "all" ? props.products : props.products.filter(prod => prod.category === category);
 
   const sortedProducts = [...filteredProducts].sort((a, b) => {
     //I created a copy of the  filteredProducts array to sort it without modifying the original
@@ -68,9 +42,19 @@ function Home() {
 
   return (
     <>
+      <Header />
+      <main>
+        <HeroBanner />
+        <div className="filters-row">
+          <CategorySelector onCategorySelect={handleCategoryFilter} />
+          <SortingDropdown sortOption={sortOption} onSortChange={handleSorting} />
+        </div>
+        <ProductGallery products={sortedProducts} />    
+      </main>
+      <Footer />
     </>
-  );
 
+  );
 
 }
 
