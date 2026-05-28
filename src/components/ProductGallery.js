863bda815dba9cbd604c1ce74924d5c34e0bee81
Luz Moreno Campos <luzdirection@gmail.com>
2026-05-28 10:48:33 -0500

    updated ProductGallery and added a product preview carousel  using the swiper package

--- src/components/ProductGallery.js
+++ src/components/ProductGallery.js
@@ -1,12 +1,37 @@
 import ProductCard from "./ProductCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+
 
 function ProductGallery(props) {
+
+  const previewProducts = props.products.slice(0, 3);
   return (
+
     <section className="product-gallery">
-      {props.products.map((product) => (
-        <ProductCard key={product.id} product={product}/>
-      ))}
-    </section>
+      <Swiper
+        modules={[Pagination]}
+        spaceBetween={20}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        className="product-gallery-carousel"
+      >
+        {previewProducts.map((product) => (
+          <SwiperSlide key={product.id}>
+            <ProductCard product={product} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      <div className="product-gallery-grid">
+        {props.products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </section >
+
   );
 }
 
