4d8686a0cf3376978eabd012571c4bce689d30d4
Olukolade <olukoladekaka@gmail.com>
2026-05-29 08:03:05 -0500

    variables

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -4,3 +4,9 @@ import { motion } from "framer-motion";
 import axios from "axios";
 import ProductDetail from "../components/ProductDetails";
 import SimilarProductGallery from "../components/SimilarProductsGallery";
+
+const pageVariants = {
+  initial: { opacity: 0, y: 20 },
+  animate: { opacity: 1, y: 0, transition: { duration: 0.45 } },
+  exit:    { opacity: 0,       transition: { duration: 0.25 } },
+};
