70f99b2da8b8866a928a7b3f66e568970fe46187
Olukolade <olukoladekaka@gmail.com>
2026-05-29 07:54:21 -0500

    imports to cart

---
+++ src/pages/Cart.js
@@ -0,0 +1,7 @@
+import { Link } from "react-router-dom";
+import { IoCartOutline } from "react-icons/io5";
+import { FaArrowLeft } from "react-icons/fa";
+import CartItem from "../components/CartItem";
+import CartSummary from "../components/CartSummary";
+import Button from "../components/Button";
+import useCartStore from "../store/cartStore";
