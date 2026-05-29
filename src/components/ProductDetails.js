e23eafd1d96f404336643ac0df11ab3424819cf4
Olukolade <olukoladekaka@gmail.com>
2026-05-29 06:55:29 -0500

    import react dependencies

---
+++ src/components/ProductDetails.js
@@ -0,0 +1,6 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaPlus, FaMinus } from "react-icons/fa";
+import { Swiper, SwiperSlide } from "swiper/react";
+import useCartStore from "../store/cartStore";
+
