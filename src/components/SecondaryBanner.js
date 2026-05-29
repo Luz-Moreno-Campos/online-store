42cbff8569021c8237bde93a6dfebfd73d1ccb0f
Olukolade <olukoladekaka@gmail.com>
2026-05-29 07:19:46 -0500

    new code added

--- src/components/SecondaryBanner.js
+++ src/components/SecondaryBanner.js
@@ -2,7 +2,17 @@ import Button from "./Button";
 
 function SecondaryBanner () {
     return (
-        <>
-        </>
-    )
+        <section className="secondary-banner">
+            <div className="overlay">
+                <div className="banner-content">
+                    <h1>Timeless Style <br /><span>&amp; Modern Elegance</span></h1>
+                    <p>
+                        Discover premium clothing designed for confidence, comfort, and
+                        effortless everyday wear.
+                    </p>
+            </div>
+        </section>
+    );
 }
+
+export default SecondaryBanner;
