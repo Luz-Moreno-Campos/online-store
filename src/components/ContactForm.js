85682dfb91347dc3bac7fcb653e3f72ca2db862a
Olukolade <olukoladekaka@gmail.com>
2026-05-28 19:54:19 -0500

    form tag added

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,5 +2,14 @@ import { useForm } from "react-hook-form";
 import Button from "./Button";
 
 function ContactForm({ onSubmit, isSubmitting }) {
-    
+    // I'm using useForm to handle form state and validation.
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    return(
+        <form>
+            
+        </form>
+    )
 }
+
+export default ContactForm;
