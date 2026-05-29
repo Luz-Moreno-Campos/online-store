73681fafe24043fc8c27f2a1596ef2fa1c6c7b76
Olukolade <olukoladekaka@gmail.com>
2026-05-29 06:58:06 -0500

    new function created

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -4,3 +4,11 @@ import { FaPlus, FaMinus } from "react-icons/fa";
 import { Swiper, SwiperSlide } from "swiper/react";
 import useCartStore from "../store/cartStore";
 
+function ProductDetails () {
+    return (
+        <>
+        </>
+    ) 
+}
+
+export default ProductDetails;
