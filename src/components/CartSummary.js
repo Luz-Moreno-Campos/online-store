76b0b98cd480265c32008ba9ae66cd9bc196d559
Olukolade <olukoladekaka@gmail.com>
2026-05-28 23:21:24 -0500

    new codes added

--- src/components/CartSummary.js
+++ src/components/CartSummary.js
@@ -1,7 +1,16 @@
-function CartSummary() {
+function CartSummary({ items }) {
+    // calculate subtotal
+  const subtotal = items.reduce(
+    (total, item) => total + item.price * item.quantity,
+    0
+  );
+  // tax 10%
+  const tax = subtotal * 0.1;
+  const total = subtotal + tax;
+
     return(
-        <>
-        </>
+        <div>
+        </div>
     )
 }
 
