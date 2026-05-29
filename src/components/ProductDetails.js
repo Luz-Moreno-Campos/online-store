afa33086b8f34e2ba988ee1d9e650e6a985dec72
Olukolade <olukoladekaka@gmail.com>
2026-05-29 07:00:11 -0500

    new code added

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -4,7 +4,25 @@ import { FaPlus, FaMinus } from "react-icons/fa";
 import { Swiper, SwiperSlide } from "swiper/react";
 import useCartStore from "../store/cartStore";
 
-function ProductDetails () {
+function ProductDetails ({ product }) {
+     // Zustand store action
+    const addItem = useCartStore((state) => state.addItem);
+    // Local states
+    const [quantity, setQuantity] = useState(1);
+    const [size, setSize] = useState("M");
+    const [color, setColor] = useState("Black");
+    const [tab, setTab] = useState("Description");
+    const sizes = ["S", "M", "L", "XL"];
+    const colors = ["Black", "White", "Beige", "Navy"];
+
+    // Add product to cart
+    const handleAdd = () => {
+        addItem(product, quantity, size, color);
+    };
+    // Loading state
+    if (!product) {
+        return <p>Loading...</p>;
+    }
     return (
         <>
         </>
