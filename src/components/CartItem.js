9470dcd3eddd38e9b309621037d14f7b3858fb8f
Olukolade <olukoladekaka@gmail.com>
2026-05-28 23:13:52 -0500

    new codes added

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,3 +1,22 @@
 import { Link } from "react-router-dom";
 import { FaTimes } from "react-icons/fa";
 import useCartStore from "../store/cartStore";
+
+function CartItem({ item }) {
+
+  const updateQuantity = useCartStore((s) => s.updateQuantity);
+  const removeItem = useCartStore((s) => s.removeItem);
+
+  return (
+    <div className="cart-item">
+
+      {/* Product Image */}
+      <Link to={`/product/${item.id}`}>
+        <img src={item.image} alt={item.title} />
+      </Link>
+
+      {/* Info */}
+      <div>
+        <Link to={`/product/${item.id}`}>
+          <p>{item.title}</p>
+        </Link>
