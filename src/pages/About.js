712a4d082910977c885fed736828e3fe66122257
Olukolade <olukoladekaka@gmail.com>
2026-05-29 07:41:31 -0500

    new function added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,3 +1,12 @@
 import SecondaryBanner from "../components/SecondaryBanner";
 import Button from "../components/Button";
 
+function About () {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default About
